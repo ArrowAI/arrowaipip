190102b844ac482a77c8c84c2771f20c1ffc19b7
Ujjwal Shukla <ujj.shukla@gmail.com>
2016-10-21 17:36:04 +0530

    asd

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'ArrowAI Package',
   author = 'ujjwal shukla',
   author_email = 'ujj.shukla@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/ArrowAI/arrowaipip', # use the URL to the github repo
+  download_url = 'https://github.com/ArrowAI/arrowaipip/tarball/0.2', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
